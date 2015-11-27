8963c6a6fde09a61b6212c23ec2d0bf4b8f3b286
Sergey Vershinin <svershinin@wiley.com>
2015-11-27 19:57:22 +0300

    adding swagger gui at swagger-ui.html

--- swagger-spring-ui/src/main/java/org/sversh/ssui/config/WebConfig.java
+++ swagger-spring-ui/src/main/java/org/sversh/ssui/config/WebConfig.java
@@ -3,6 +3,7 @@ package org.sversh.ssui.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
@@ -16,4 +17,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Import(SwaggerConfig.class)
 public class WebConfig extends WebMvcConfigurerAdapter {
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+    
+    
 }
