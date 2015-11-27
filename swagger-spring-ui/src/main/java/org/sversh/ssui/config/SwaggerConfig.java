6b62cbe07d4566e28228c15c4907984f028d2ee2
Sergey Vershinin <svershinin@wiley.com>
2015-11-27 19:28:36 +0300

    adding api-doc generation in json format at v2/api-docs

---
+++ swagger-spring-ui/src/main/java/org/sversh/ssui/config/SwaggerConfig.java
@@ -0,0 +1,46 @@
+package org.sversh.ssui.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * 
+ * @author Sergey Vershinin</a>
+ * @since Oct 23, 2015
+ *
+ */
+@Configuration
+@EnableSwagger2
+@ComponentScan(basePackages = { "com.wiley.funderservice.ws.controllers" })
+public class SwaggerConfig {
+    
+    @Bean
+    public Docket restfulApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                //.groupName("restful-api")
+                //.select()
+                //.build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+
+        ApiInfo apiInfo = new ApiInfo(
+                "My Apps API Title",
+                "My Apps API Description",
+                "My Apps API Version",
+                "My Apps API terms of service",
+                "My Apps API Contact Email",
+                "My Apps API Licence Type",
+                "My Apps API License URL"
+        );
+        return apiInfo;
+    }
+
+}
--- swagger-spring-ui/src/main/java/org/sversh/ssui/config/WebConfig.java
+++ swagger-spring-ui/src/main/java/org/sversh/ssui/config/WebConfig.java
@@ -1,6 +1,7 @@
 package org.sversh.ssui.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -12,6 +13,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  */
 @Configuration
 @EnableWebMvc
+@Import(SwaggerConfig.class)
 public class WebConfig extends WebMvcConfigurerAdapter {
 
 }
